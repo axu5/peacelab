ac6561294bd8947cf13f98cfe6844c932c40615b
Axu <321axu123@gmail.com>
2023-04-03 23:23:46 +0200

    lesson order?

--- helpers/getLessonMetadata.ts
+++ helpers/getLessonMetadata.ts
@@ -1,35 +1,68 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { MarkdownMetadata } from "./MarkdownMetadata";
 import { folderLocation } from "@/constants/tutorial";
+// Order of chapters
+const chapterOrder = [
+    "Site Navigation",
+    "Googling",
+    "Editing Documents",
+    "Data Management",
+    "Cyber Security",
+] as const;
 
-export default function getMarkdownMetadata(): MarkdownMetadata[][] {
+// type chapterNamesType = typeof chapterOrder[number];
+
+export type MarkdownFileMetadata = {
+    title: string;
+    subtitle: string;
+};
+
+export type MarkdownMetadata = {
+    name: string;
+    key: number;
+    lessons: ({
+        id: string;
+        path: string;
+        lesson: string;
+    } & MarkdownFileMetadata)[];
+};
+
+export default function getMarkdownMetadata(): MarkdownMetadata[] {
     const chapters = fs.readdirSync(folderLocation);
-
+    const order = Object.fromEntries(
+        chapterOrder.map((chapterName, i) => [chapterName, i])
+    );
     // Get gray-matter data from each file.
-    const paths = chapters.map(chapter => {
+    const paths = chapters.map(chapterName => {
         const lessons = fs.readdirSync(
-            path.join(folderLocation, chapter)
+            path.join(folderLocation, chapterName)
         );
-        return lessons.map(fileName => {
-            const filePath = `${folderLocation}/${chapter}/${fileName}`;
-            const fileContents = fs.readFileSync(filePath, "utf8");
-            const matterResult = matter(fileContents);
-            const lesson = fileName.replace(".md", "");
-            return {
-                id: chapter + "-" + lesson,
-                title: matterResult.data.title,
-                subtitle: matterResult.data.subtitle,
-                path: `${folderLocation}/${chapter}/${lesson}`.replaceAll(
-                    " ",
-                    "-"
-                ),
-                lesson: lesson,
-                chapter,
-            } satisfies MarkdownMetadata;
-        });
+
+        return {
+            name: chapterName,
+            key: order[chapterName],
+            lessons: lessons.map(fileName => {
+                const filePath = `${folderLocation}/${chapterName}/${fileName}`;
+                const fileContents = fs.readFileSync(
+                    filePath,
+                    "utf8"
+                );
+                const { data } = matter(fileContents);
+                const lesson = fileName.replace(".md", "");
+                return {
+                    id: chapterName + "-" + lesson,
+                    title: data.title,
+                    subtitle: data.subtitle,
+                    path: `${folderLocation}/${chapterName}/${lesson}`.replaceAll(
+                        " ",
+                        "-"
+                    ),
+                    lesson: lesson,
+                };
+            }),
+        } satisfies MarkdownMetadata;
     });
 
-    return paths;
+    return paths.sort((a, b) => a.key - b.key);
 }
